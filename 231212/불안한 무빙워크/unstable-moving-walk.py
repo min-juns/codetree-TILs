3d89879f89aaaa053680e2203afd0c912f5ad6c5
min-juns <85685355+min-juns@users.noreply.github.com>
2023-12-12 22:26:03 +0900

    Title: 불안한 무빙워크, Time: 676ms, Memory: 26MB, Status: Passed - CodeTree

--- 231212/불안한 무빙워크/unstable-moving-walk.py
+++ 231212/불안한 무빙워크/unstable-moving-walk.py
@@ -4,6 +4,7 @@ N, k = map(int, input().split())
 belt = deque(list(map(int, input().split())))
 people = deque([False for _ in range(N * 2)])
 
+
 count = 0
 while True:
     if belt.count(0) >= k:
@@ -18,10 +19,11 @@ while True:
         people[N] = False
 
     # step 2
-    for n in range(N*2):
-        if n == N-1 or n == (N*2 - 1):
-            continue
-        if people[n]:
+    for n in range(N-1, -1, -1):
+        if n == N-1:
+            if people[n]:
+                people[n] = False
+        elif people[n]:
             if (not people[n+1]) and belt[n+1] != 0:
                 people[n] = False
                 people[n+1] = True
@@ -31,7 +33,7 @@ while True:
     if not people[0] and belt[0] != 0:
         people[0] = True
         belt[0] -= 1
-
+    
     # step 4
     if belt.count(0) >= k:
         break
