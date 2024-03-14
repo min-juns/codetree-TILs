19778a4a2e49938c0b473ceb18f189965a3b0ccf
min-juns <85685355+min-juns@users.noreply.github.com>
2024-03-14 23:29:57 +0900

    Title: 놀이기구 탑승, Time: 158ms, Memory: 26MB, Status: Passed - CodeTree

--- 240314/놀이기구 탑승/go-on-the-rides.py
+++ 240314/놀이기구 탑승/go-on-the-rides.py
@@ -3,7 +3,7 @@ game_map = [[0 for _ in range(N)] for _ in range(N)]
 like_list = [[] for _ in range(N ** 2 + 1)]
 person_order = []
 
-for _ in range(N * N):
+for _ in range(N ** 2):
     n0, n1, n2, n3, n4 = map(int, input().split())
     like_list[n0] = [n1, n2, n3, n4]
     person_order.append(n0)
@@ -11,24 +11,27 @@ for _ in range(N * N):
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 def select_place(p_id):
-    global game_map
     p_like_list = like_list[p_id]
     best_like_friends, best_empty_space = 0, 0
-    best_r, best_c = -1, -1
-
+    best_r, best_c = -1e+9, -1e+9
+    first_zero = True
     for r in range(N):
         for c in range(N):
             if game_map[r][c] != 0:
                 continue
+            if first_zero:
+                best_r, best_c = r, c
+                first_zero = False
             like_friends = 0
             empty_space = 0
-            for i in range(4):
-                nr, nc = r + dr[i], c + dc[i]
+            for k in range(4):
+                nr, nc = r + dr[k], c + dc[k]
                 if nr < 0 or nc < 0 or nr >= N or nc >= N:
                     continue
-                if game_map[nr][nc] == 0:
+                p_idx = game_map[nr][nc]
+                if p_idx == 0:
                     empty_space += 1
-                elif game_map[nr][nc] in p_like_list:
+                elif p_idx in p_like_list:
                     like_friends += 1
             if like_friends > best_like_friends:
                 best_like_friends = like_friends
@@ -45,15 +48,16 @@ def select_place(p_id):
 for po in person_order:
     select_place(po)
 
+
 score = [0, 1, 10, 100, 1000]
 total_score = 0
-for r in range(N):
-    for c in range(N):
-        selected_p = game_map[r][c]
+for qr in range(N):
+    for qc in range(N):
+        selected_p = game_map[qr][qc]
         friend_list = like_list[selected_p]
         num_friend = 0
         for i in range(4):
-            nr, nc = r + dr[i], c + dc[i]
+            nr, nc = qr + dr[i], qc + dc[i]
             if nr < 0 or nc < 0 or nr >= N or nc >= N:
                 continue
             if game_map[nr][nc] in friend_list:
