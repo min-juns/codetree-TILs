c9bb399aae15aa56930ac1f359af8c36c12b3088
min-juns <85685355+min-juns@users.noreply.github.com>
2024-03-19 23:48:10 +0900

    Title: 2개의 사탕, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240319/2개의 사탕/two-candies.py
+++ 240319/2개의 사탕/two-candies.py
@@ -9,118 +9,10 @@ for n in range(N):
         elif game_map[n][m] == "B":
             blue_candy = (n, m)
 
-
-"""
-def move_pos(d):
-    global game_map
-    # 오른쪽으로 기울이는 경우
-    next_game_map = []
-    if d == 0:
-        for n in range(N):
-            game_list = game_map[n]
-            next_game_list = ["." for _ in range(M)]
-            temp_list = []
-            for m in range(M):
-                if game_list[m] == "B" or game_list[m] == "R":
-                    temp_list.append(game_list[m])
-                elif game_list[m] == "#":
-                    next_game_list[m] = "#"
-                    if len(temp_list) > 0:
-                        for l in range(len(temp_list)):
-                            next_game_list[m - l - 1] = temp_list[-l - 1]
-                        temp_list = []
-                elif game_list[m] == "O":
-                    next_game_list[m] = "O"
-                    if len(temp_list) == 2:
-                        return "fail"
-                    elif len(temp_list) == 1:
-                        if temp_list[0] == "R":
-                            return "success"
-                        else:
-                            return "fail"
-            next_game_map.append(next_game_list)
-    # 아래로 이동
-    elif d == 1:
-        next_game_map = [["." for _ in range(M)] for _ in range(N)]
-        for m in range(M):
-            temp_list = []
-            for n in range(N):
-                if game_map[n][m] == "B" or game_map[n][m] == "R":
-                    temp_list.append(game_map[n][m])
-                elif game_map[n][m] == "#":
-                    next_game_map[n][m] = "#"
-                    if len(temp_list) > 0:
-                        for l in range(len(temp_list)):
-                            next_game_map[n - l - 1][m] = temp_list[-l - 1]
-                        temp_list = []
-                elif game_map[n][m] == "O":
-                    next_game_map[n][m] = "O"
-                    if len(temp_list) == 2:
-                        return "fail"
-                    elif len(temp_list) == 1:
-                        if temp_list[0] == "R":
-                            return "success"
-                        else:
-                            return "fail"
-    # 왼쪽으로 이동
-    elif d == 2:
-        for n in range(N):
-            game_list = game_map[n]
-            next_game_list = ["." for _ in range(M)]
-            temp_list = []
-            for m in range(M-1, -1, -1):
-                if game_list[m] == "B" or game_list[m] == "R":
-                    temp_list.append(game_list[m])
-                elif game_list[m] == "#":
-                    next_game_list[m] = "#"
-                    if len(temp_list) > 0:
-                        for l in range(len(temp_list)):
-                            next_game_list[m + l + 1] = temp_list[-l - 1]
-                        temp_list = []
-                elif game_list[m] == "O":
-                    next_game_list[m] = "O"
-                    if len(temp_list) == 2:
-                        return "fail"
-                    elif len(temp_list) == 1:
-                        if temp_list[0] == "R":
-                            return "success"
-                        else:
-                            return "fail"
-            next_game_map.append(next_game_list)
-
-    # 위로 이동
-    elif d == 3:
-        next_game_map = [["." for _ in range(M)] for _ in range(N)]
-        for m in range(M):
-            temp_list = []
-            for n in range(N-1, -1, -1):
-                if game_map[n][m] == "B" or game_map[n][m] == "R":
-                    temp_list.append(game_map[n][m])
-                elif game_map[n][m] == "#":
-                    next_game_map[n][m] = "#"
-                    if len(temp_list) > 0:
-                        for l in range(len(temp_list)):
-                            next_game_map[n + l + 1][m] = temp_list[-l - 1]
-                        temp_list = []
-                elif game_map[n][m] == "O":
-                    next_game_map[n][m] = "O"
-                    if len(temp_list) == 2:
-                        return "fail"
-                    elif len(temp_list) == 1:
-                        if temp_list[0] == "R":
-                            return "success"
-                        else:
-                            return "fail"
-    game_map = next_game_map
-            
-    return "go"
-"""
-
 def move_pos(d):
     global red_candy
     global blue_candy
     # 오른쪽으로 기울이는 경우
-    next_game_map = []
     same_pos = False
     red_first = False
     # 좌우로 움직이는 경우
@@ -144,8 +36,8 @@ def move_pos(d):
                         red_first = True
     # 위아래로 움직이는 경우
     elif d == 1 or d == 3:
-        same_pos = True
         if red_candy[1] == blue_candy[1]:
+            same_pos = True
             if d == 1:
                 for k in range(min(red_candy[0], blue_candy[0]), max(red_candy[0], blue_candy[0]) + 1):
                     if game_map[k][red_candy[1]] == "#" or game_map[k][red_candy[1]] == "O":
@@ -245,13 +137,6 @@ def find_min_num(num):
             blue_candy = copy.deepcopy(current_b_candy)
             red_candy = copy.deepcopy(current_r_candy)
 
-"""
-move_pos(1)
-print(red_candy)
-print(blue_candy)
-print("###################")
-
-"""
 find_min_num(0)
 if min_num == 10:
     print(-1)
